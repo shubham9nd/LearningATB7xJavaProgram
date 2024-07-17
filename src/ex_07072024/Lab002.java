b7a33aeab2edb55663f63aa62f63e72d1fbed248
Shubham Sharma <shubhamnarad09@gmail.com>
2024-07-17 18:29:27 +0530

    Initial commit

---
+++ src/ex_07072024/Lab002.java
@@ -0,0 +1,12 @@
+package ex_07072024;
+
+public class Lab002 {
+    public static void main(String[] args)
+{
+    System.out.println("Hello world!");
+    System.out.println("Hello Sachin!");
+    System.out.println("Hello Neeru!");
+    System.out.println("Hello Shaan!");
+    System.out.println("Hello Golu!");
+}
+}
